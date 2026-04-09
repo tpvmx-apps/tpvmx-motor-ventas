23f1cb15cf7ba6b1225635311002e5138bec0270
tpvmx-apps <albertoe.ubg@gmail.com>
2026-04-09 00:59:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -417,3 +417,20 @@ function sendFile(res, filePath) {
 
   stream.pipe(res);
 }
+
+function getMimeType(filePath) {
+  const extension = path.extname(filePath).toLowerCase();
+
+  switch (extension) {
+    case ".html":
+      return "text/html; charset=utf-8";
+    case ".css":
+      return "text/css; charset=utf-8";
+    case ".js":
+      return "application/javascript; charset=utf-8";
+    case ".json":
+      return "application/json; charset=utf-8";
+    default:
+      return "application/octet-stream";
+  }
+}
